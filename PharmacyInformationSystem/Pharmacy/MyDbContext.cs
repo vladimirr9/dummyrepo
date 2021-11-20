ff588f3dcd6246ac24410d2c0c3b66523409c0f9
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-11-20 17:05:49 +0100

    test

--- PharmacyInformationSystem/Pharmacy/MyDbContext.cs
+++ PharmacyInformationSystem/Pharmacy/MyDbContext.cs
@@ -24,7 +24,7 @@ namespace PharmacyClassLib
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            String connectionString = "Server=localhost; Port =5432; Database=Pharmacy; User Id=postgres; Password =pass;";
+            String connectionString = "Server=localhost; Port =5432; Database=Pharmacy; User Id=postgres; Password =root;";
             optionsBuilder.UseNpgsql(connectionString);
         }
 
